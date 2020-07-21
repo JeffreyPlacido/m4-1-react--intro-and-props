2e45136b0677f7b6602a5ad53747c880d12bd190
Jeffrey <jeffreyplacido@hotmail.com>
2020-07-21 11:27:15 -0400

    E4

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,10 @@ const App = (props) => {
   console.log(props);
   return (
     <div className='wrapper'>
-      <Header />
+      <Header
+        participants={props.conversation.participants}
+        currentUser={props.currentUser}
+      />
       <ChatStream messages={props.conversation.messages}
       currentUser={props.currentUser}/>
       <Footer />
